7f5e94715d4621ce3dec4b90946b42bd3a1e9a1a
Brian Bagdasarian <brian@aperfectshirt.com>
2013-08-22 19:51:45 -0400

    end of thursday - working on wizard

---
+++ js/lib/livibutton.js
@@ -0,0 +1,38 @@
+$('.kuewtype').addLivicon({
+ 
+  size : 50,
+  color : "white",
+  hovercolor : "gold",
+  animate : "true",
+  loop : false,
+  iteration : 3,
+  duration : 700,
+  eventtype : "hover",
+  onparent : false
+  });
+  
+$('.nav-left').addLivicon({
+  name: "arrow-left",
+  size : 50,
+  color : "white",
+  hovercolor : "gold",
+  animate : "true",
+  loop : false,
+  iteration : 3,
+  duration : 700,
+  eventtype : "hover",
+  onparent : false
+  });
+  
+$('.nav-right').addLivicon({
+  name: "arrow-right",
+  size : 50,
+  color : "white",
+  hovercolor : "gold",
+  animate : "true",
+  loop : false,
+  iteration : 3,
+  duration : 700,
+  eventtype : "hover",
+  onparent : false
+  });
