85ae12ed669388a4a54c1edee4ab4b1f7a70def3
bags307 <brian@aperfectshirt.com>
2013-09-28 22:17:35 -0400

    remember to use the css transitions from dash-style

--- js/lib/loader/loader.js
+++ js/lib/loader/loader.js
@@ -1,8 +1,8 @@
 $(window).load(function() {       
              
-    $('#loading').toggleClass("animated").toggleClass("flipOutY").hide().toggleClass("flex", function(){        
-    $('#container').toggleClass("flex").toggleClass("flex-vert").toggleClass("animated").toggleClass("rotateInDownLeft").show("slow", function(){
-    	$("#stickyfooter").toggleClass("animated").toggleClass("rotateInDownRight").show("slow");
+    $('#loading').toggleClass("animated").toggleClass("flipOutY").hide().toggleClass("flex", function(){
+    $('#container').toggleClass("flex").toggleClass("flex-vert").toggleClass("animated").toggleClass("bounceIn").show("slow", function(){
+    	$("#stickyfooter").toggleClass("animated").toggleClass("bounceIn").show("slow");
     }); 
     $('.wizards').toggleClass("openpage");
   });
